0a8f998f2ef34329867084c4f4720c8520180caf
Andrew Colin Kissa <andrew@topdog.za.net>
2015-11-16 15:54:57 +0200

    * Added CLI tests

--- tests/test-cli.py
+++ tests/test-cli.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 import mock
@@ -8,11 +9,39 @@ except ImportError:
         raise
     import unittest as unittest2
 
+from sachannelupdate.cli import main
+from sachannelupdate.exceptions import SaChannelUpdateConfigError
+
 
 class CLITestCase(unittest2.TestCase):
 
-    def test_main(self):
-        pass
+    def test_main_no_cf(self):
+        default_cfg = '/etc/sachannelupdate/sachannelupdate.ini'
+        sys.argv = ['__main__', '-c', default_cfg]
+        with self.assertRaises(SaChannelUpdateConfigError) as cma:
+            main()
+            self.assertEqual(
+                cma.exception.message,
+                'The configuration file: %s does not exist' % default_cfg
+            )
+
+    @mock.patch('sachannelupdate.cli.entry')
+    def test_main_cf(self, mock_entry):
+        config = os.path.join(os.path.dirname(__file__), 'sa.ini')
+        sys.argv = ['__main__', '-c', config]
+        main()
+        self.assertTrue(mock_entry.called)
+
+    # @mock.patch('sachannelupdate.cli.entry')
+    # def test_main_bad_cf(self, mock_entry):
+    #     config = os.path.join(
+    #         os.path.dirname(__file__),
+    #         'bad-sachannelupdate.ini'
+    #     )
+    #     sys.argv = ['__main__', '-c', config]
+    #     # with self.assertRaises(BaseException) as cma:
+    #     main()
+    #     # self.assertTrue(mock_entry.called)
 
 
 if __name__ == "__main__":
